66d657b93457f424e7b3a45f85d8d51de75b418c
rashid alkhaili <r@myrsk.com>
2019-10-18 15:01:19 +0400

    helloworld
    helloworld

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Plugin Installed!"; #NOT! :)
+
+cd $DOCUMENT_ROOT; //this directory
+cd ..
+for dir in user reseller admin; do
+{
+        chmod 755 $dir/*
+        chown diradmin:diradmin $dir/*
+}
+done;
+
+exit 0;
